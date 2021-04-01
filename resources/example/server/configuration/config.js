373c61462a29a3e5c860cb91b8b8b7d3e10a1798
Ramazan Hoyladı <hoylram@gmail.com>
2021-04-01 15:26:26 +0300

    Resource ismi değiştirildi

--- resources/example/server/configuration/config.js
+++
@@ -1,9 +0,0 @@
-export const DEFAULT_CONFIG = {
-    SPAWN: {
-        x: -1291.7142333984375,
-        y: 83.43296813964844,
-        z: 54.8916015625
-    },
-    SPAWN_RANGE: 10,
-    RANDOM_PLAYER_MODELS: ['mp_f_freemode_01', 'mp_m_freemode_01']
-};
